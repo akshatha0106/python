10afcbbdc89f91b0cbd142ad10a67fece98e6ae1
akshatha0106 <akshathaa0106@gmail.com>
2025-08-01 00:28:24 +0530

    p4.py

--- p4.py
+++ p4.py
@@ -7,17 +7,17 @@ def digital_clock():
     label.after(1000, digital_clock)
 
 
-root = Tk()
-root.title("Digital Clock")
-root.geometry("620x150")
-root.configure(bg="black")
+dc = Tk()
+dc.title("Digital Clock")
+dc.geometry("620x150")
+dc.configure(bg="black")
 
 # Create a label to display time
-label = Label(root, font=("Arial", 60), bg="black", fg="cyan", bd=25)
+label = Label(dc, font=("Arial", 60), bg="black", fg="cyan", bd=25)
 label.pack()
 
 # Start updating the clock
 digital_clock()
 
 # Run the GUI event loop
-root.mainloop()
+dc.mainloop()
